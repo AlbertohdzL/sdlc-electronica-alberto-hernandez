47cf4609139404c678056ff0babf36e24a8db7ae
Alberto Hernández Landa <albertolanda489@mail.com>
2026-07-08 14:45:22 -0600

    feat: checkpoint semana 0 - sensor minimo con test

---
+++ semana0/hola_sensor.py
@@ -0,0 +1,4 @@
+class Sensor:
+    def read(self) -> float:
+        # Devuelve el valor fijo solicitado por el checkpoint
+        return 23.5
---
+++ semana0/test_hola_sensor.py
@@ -0,0 +1,5 @@
+from hola_sensor import Sensor
+
+def test_sensor_read():
+    sensor = Sensor()
+    assert sensor.read() == 23.5
